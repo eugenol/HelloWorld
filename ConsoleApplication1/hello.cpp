eedf9c312c845b1987451abef131e574ea4d46ea
Eugene <eug@live.com>
2015-02-25 15:22:12 +0200

    add goodnight message

--- ConsoleApplication1/hello.cpp
+++ ConsoleApplication1/hello.cpp
@@ -1,15 +1,15 @@
-// Firdt c++ program (Hello, world)
+// First c++ program (Hello, world)
 // Eugene, CM3 2015, Tut1 25/02/2015
 
 #include <iostream>
 #include <string>
-
-using namespace std;
 
 int main() {
 	std::string myName;
 	std::cout << "Please enter your name: ";
 	std::cin >> myName;
 	std::cout << "Hello, "<< myName << std::endl;
+
+	std::cout << "Goodnight" << std::endl;
 	return 0;
 }
